728bd7208df2a495719777bf520742e9590e2a6e
why <529570509@qq.com>
2019-09-16 09:00:55 +0800

    starter

---
+++ springbootpractice/src/main/java/com/why/springbootpractice/controllers/UserController.java
@@ -0,0 +1,32 @@
+package com.why.springbootpractice.controllers;
+
+import com.why.springbootpractice.pojos.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by wanghaiyang on 2019/9/16.
+ */
+@Controller
+public class UserController {
+
+    @RequestMapping("/userpojo")
+    @ResponseBody
+    public User userPojo() {
+        Calendar c = Calendar.getInstance();
+        c.set(c.YEAR, 1993);
+        c.set(c.MONTH, 10);
+        c.set(c.DAY_OF_MONTH, 19);
+        User user = new User();
+        user.setName("Ethan");
+        user.setAge(25);
+        user.setBirthday(c.getTime());
+        user.setPassword("123456");
+        user.setDesc("This is Ethan's user pojo");
+        return user;
+    }
+}
